7b0cedc81603e90e4de2fa77ccf1c41a624968b1
nikstar <nstarshinov@yandex.ru>
2018-04-09 14:27:31 +0300

    Update dataset_catalog.py

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -37,6 +37,14 @@ RAW_DIR = 'raw_dir'
 
 # Available datasets
 DATASETS = {
+    'kitti_train': {
+        IM_DIR:
+            _DATA_DIR + '/kitti/images',
+        ANN_FN:
+            _DATA_DIR + '/kitti/annotations/train.json',
+        RAW_DIR:
+            _DATA_DIR + '/kitti/raw'
+    }
     'cityscapes_fine_instanceonly_seg_train': {
         IM_DIR:
             _DATA_DIR + '/cityscapes/images',
